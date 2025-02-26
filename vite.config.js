10ea966c772fd804ad98b8bbcf354cff59727ab9
guniism <gun16110@hotmail.com>
2025-02-27 03:40:48 +0700

    change api target

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        // target: "http://localhost:5000",
+        target: "https://antony-massage-backend-production.up.railway.app/",
         changeOrigin: true,
         secure: false,
       },
