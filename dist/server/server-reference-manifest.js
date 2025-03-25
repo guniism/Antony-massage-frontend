7344c3210293ffec1bf6c7afc39aeafc878dd81d
guniism <gun16110@hotmail.com>
2025-03-25 10:01:28 +0700

    For deploy

--- dist/server/server-reference-manifest.js
+++ dist/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"63d0JfP9fp9Y+O4pzHszBQ+1CGmgStkjQJ0pIXtFZSY=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  distDir: 'dist', // Set the output directory to "dist"
 };
 
 export default nextConfig;
